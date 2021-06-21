43c0061fcd42f1509f9e8a32a3f70003807a0ca5
Axel Padilla <adpadillar25@gmail.com>
2021-06-21 12:29:09 -0600

    WebSockets improvements

--- client/src/components/expressTest/ConnectSocket.tsx
+++ client/src/components/expressTest/ConnectSocket.tsx
@@ -1,48 +1,64 @@
 import React from "react";
-import { useEffect } from "react";
 import { useState } from "react";
 
+interface wsDataType {
+  currentData: string;
+  roomId: string;
+  isConnected?: boolean;
+}
+
 const ConnectSocket: React.FC = () => {
-  const [serverMsg, setServerMsg] = useState("");
-  const [isConnected, setIsConnected] = useState(false);
+  const [roomData, setRoomData] = useState<wsDataType>({
+    roomId: "",
+    currentData: "",
+    isConnected: false,
+  });
   const [webSocket, setWebSocket] = useState<WebSocket>();
 
   const connect = () => {
     const ws = new WebSocket("ws://localhost:8080");
-    ws.addEventListener("open", (e) => {
-      setIsConnected(true);
+    setWebSocket(ws);
+
+    ws.addEventListener("open", () => {
+      ws.send(JSON.stringify(roomData));
     });
 
-    ws.addEventListener("message", (e) => setServerMsg(e.data));
-
-    return ws;
+    ws.addEventListener("message", ({ data }) => {
+      console.log(data);
+      setRoomData(JSON.parse(data));
+    });
   };
 
-  useEffect(() => {
-    setWebSocket(connect());
-  }, []);
-
-  const updateMsg = (newValue: string) => {
-    if (webSocket) {
-      webSocket.send(newValue);
-      setServerMsg(newValue);
-    }
+  const updateData = (newData: wsDataType) => {
+    webSocket?.send(JSON.stringify(newData));
   };
 
   return (
     <>
-      <h1>Web Sockets</h1>
-      {!isConnected ? (
-        <p>Loading...</p>
+      {roomData.isConnected ? (
+        <div>
+          <h1>Room {roomData.roomId}</h1>
+          <textarea
+            value={roomData.currentData}
+            onChange={({ target }) =>
+              updateData({ ...roomData, currentData: target.value })
+            }
+          ></textarea>
+          <p>{roomData.currentData}</p>
+        </div>
       ) : (
         <div>
-          <textarea
-            value={serverMsg}
-            onChange={(e) => updateMsg(e.target.value)}
+          <h1>Connect to a room</h1>
+          <input
+            value={roomData.roomId}
+            onChange={({ target }) =>
+              setRoomData({ ...roomData, roomId: target.value })
+            }
           />
+          <button onClick={connect}>Connect!</button>
         </div>
       )}
-      <p>{serverMsg}</p>
+      <pre>{JSON.stringify(roomData, null, 2)}</pre>
     </>
   );
 };
--- server.js
+++ server.js
@@ -9,18 +9,74 @@ const server = require("http").createServer(app);
 const ws = require("ws");
 const wss = new ws.Server({ server: server });
 
-//WS handler
-wss.on("connection", (ws) => {
-  ws.on("message", (msg) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === ws.OPEN) {
-        client.send(msg);
+const Rooms = [];
+
+/**A WebSocket room identified with a unique id */
+class Room {
+  /**Initialize the Room with an Id */
+  constructor(id) {
+    this.id = id;
+    this.clients = [];
+    this.currentData = {
+      roomId: id,
+      currentData: "",
+      isConnected: true,
+    };
+    console.log(`Created room ${id}`);
+    return this;
+  }
+
+  /**Takes a client and adds it to the Room's clients. Updates the client with most
+   * recent data and set's up an event listener */
+  addClient(client) {
+    this.clients.push(client);
+    this.sendUpdate(client);
+    client.on("message", (data) => {
+      this.broadcast(JSON.parse(data));
+    });
+    client.on("close", () => {
+      client.removeAllListeners();
+    });
+    console.log(`Added client to room ${this.id}`);
+    return this;
+  }
+
+  /**Takes a client and sends the current Room Data to it as a string */
+  sendUpdate(client) {
+    client.send(JSON.stringify(this.currentData));
+    return this;
+  }
+
+  /** Takes parsed JSON, sets it as current Room Data and sends it to all clients */
+  broadcast(data) {
+    this.currentData = data;
+    this.clients.forEach((client) => {
+      if (client.readyState === client.OPEN) {
+        client.send(JSON.stringify(this.currentData));
       }
     });
-  });
+    return this;
+  }
+}
 
-  ws.on("close", (idx) => {
-    console.log(`Client ${idx} disconnected`);
+//WS handler
+wss.on("connection", (client) => {
+  client.on("message", (data) => {
+    const parsedData = JSON.parse(data);
+    // New client
+    if (!parsedData.isConnected) {
+      if (Rooms.length === 0) {
+        Rooms.push(new Room(parsedData.roomId).addClient(client));
+      } else {
+        Rooms.forEach((r) => {
+          if (r.id === parsedData.roomId) {
+            r.addClient(client);
+          } else {
+            Rooms.push(new Room(parsedData.roomId).addClient(client));
+          }
+        });
+      }
+    }
   });
 });
 
